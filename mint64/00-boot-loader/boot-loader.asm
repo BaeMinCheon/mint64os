18959d975c1d48a6e72c1b9eb87e536d4e7bf31d
baemincheon <baemincheon@gmail.com>
2024-01-01 23:17:11 +0900

    The chapter 5.2

--- mint64/00-boot-loader/boot-loader.asm
+++ mint64/00-boot-loader/boot-loader.asm
@@ -11,6 +11,10 @@ START:
     mov ds, ax
     mov ax, 0xB800
     mov es, ax
+    mov ax, 0x0000
+    mov ss, ax
+    mov sp, 0xFFFE
+    mov bp, 0xFFFE
     mov si, 0
 
 .SCREENCLEARLOOP:
@@ -19,11 +23,91 @@ START:
     add si, 2
     cmp si, 80*25*2
     jl .SCREENCLEARLOOP
-    mov si, 0
-    mov di, 0
+    push MESSAGE1
+    push 0
+    push 0
+    call PRINTMESSAGE
+    add sp, 6
+    push IMAGELOADINGMESSAGE
+    push 1
+    push 0
+    call PRINTMESSAGE
+    add sp, 6
+
+RESETDISK:
+    mov ax, 0
+    mov dl, 0
+    int 0x13
+    jc HANDLEDISKERROR
+    mov si, 0x1000
+    mov es, si
+    mov bx, 0x0000
+    mov di, word [ TOTALSECTORCOUNT ]
+
+READDATA:
+    cmp di, 0
+    je READEND
+    sub di, 0x1
+    mov ah, 0x02
+    mov al, 0x1
+    mov ch, byte [ TRACKNUMBER ]
+    mov cl, byte [ SECTORNUMBER ]
+    mov dh, byte [ HEADNUMBER ]
+    mov dl, 0x00
+    int 0x13
+    jc HANDLEDISKERROR
+    add si, 0x0020
+    mov es, si
+    mov al, byte [ SECTORNUMBER ]
+    add al, 0x01
+    mov byte [ SECTORNUMBER ], al
+    cmp al, 19
+    jl READDATA
+    xor byte [ HEADNUMBER ], 0x01
+    mov byte [ SECTORNUMBER ], 0x01
+    cmp byte [ HEADNUMBER ], 0x00
+    jne READDATA
+    add byte [ TRACKNUMBER ], 0x01
+    jmp READDATA
+
+READEND:
+    push IMAGELOADINGCOMPLETEMESSAGE
+    push 2
+    push 0
+    call PRINTMESSAGE
+    add sp, 6
+    jmp 0x1000:0x0000
+
+HANDLEDISKERROR:
+    push DISKERRORMESSAGE
+    push 2
+    push 0
+    call PRINTMESSAGE
+    jmp $
+
+PRINTMESSAGE:
+    push bp
+    mov bp, sp
+    push es
+    push si
+    push di
+    push ax
+    push cx
+    push dx
+    mov ax, 0xB800
+    mov es, ax
+    mov ax, word [ bp + 6 ]
+    mov si, 160
+    mul si
+    mov di, ax
+    mov ax, word [ bp + 4 ]
+    mov si, 2
+    mul si
+    add di, ax
+    mov si, word [ bp + 8 ]
 
 .MESSAGELOOP:
-    mov cl, byte [ si+MESSAGE1 ]
+    mov cl, byte [ si ]
     cmp cl, 0
     je .MESSAGEEND
     mov byte [ es : di ], cl
@@ -32,9 +116,23 @@ START:
     jmp .MESSAGELOOP
 
 .MESSAGEEND:
-    jmp $
+    pop dx
+    pop cx
+    pop ax
+    pop di
+    pop si
+    pop es
+    pop bp
+    ret
 
-MESSAGE1: db '[Starting Mint64 OS boot loader]', 0
+MESSAGE1: db '[Starting Mint64 OS boot loader...]', 0
+DISKERRORMESSAGE: db '[Error with disk operations]', 0
+IMAGELOADINGMESSAGE: db '[Starting to load the operating system image...]', 0
+IMAGELOADINGCOMPLETEMESSAGE: db '[Finished to load the operating system image]', 0
+TOTALSECTORCOUNT: dw 1024
+SECTORNUMBER: db 0x02
+HEADNUMBER: db 0x00
+TRACKNUMBER: db 0x00
 
 times 510 - ($-$$) db 0x00
 db 0x55
